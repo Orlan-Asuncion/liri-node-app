5828dbfa6d640dd3d562e5056c64965bf68940ec
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-06-13 12:26:35 -0400

    updated REadMe.md file

--- README.md
+++ README.md
@@ -1,39 +1,40 @@
-ABOUT THE APP
+## ABOUT THE APP
 
 LIRI is a Language Interpretation and Recognition Interface, it's just like SIRI(Speech Interpretation and Recognition Interface).LIRI is a node.js command line that gathers information(parameters) and returns back data.In this application, user has the option to use the command lines listed below;
+
 * concert-this
 * spotify-this-song
 * movie-this
 * do-what-it-says
 
-HOW LIRI WORKS
+## HOW LIRI WORKS
 
-Step by Step instructions
+### Step by Step instructions
 
 First, navigate the LIRI node app folder and open the file liri.js.
 Second, open liri.js in the bash terminal.
 Third, on the command line enter the command you want to use.
 
-       Example: Run  "concert-this" command
+    *   Example: Run  "concert-this" command
 
-       $ node liri.js concert-this <name of artist or band>
+       $ node _liri.js_  concert-this <name of artist or band>
 
        Output: The system will display "List of Events": 'Name of the Venue','Venue Location' and 'Date of the Event'.
 
-       ![Results](/screenshots/concert-this_results.PNG)
+       ![](screenshots/concert-this_results.PNG)
 
 Example: Run  "spotify-this-song" command
 
-       $ node liri.js spotify-this-song <name of song>
+    $ node _liri.js_ spotify-this-song <name of song>
 
        Output: The system will display : Artist(s),The song's name, A preview link of the song from Spotify,and The album that the song is from. 
 
-     ![Results](/screenshots/spotify-this-song_results.PNG)
+     ![](screenshots/spotify-this-song_results.PNG)
         
       
 Example: Run  "movie-this" command
 
-       $ node liri.js movie-this <name of movie>
+    $ node _liri.js_ movie-this <name of movie>
 
         Output: The system will display information associated with the movie such as;
 
@@ -46,20 +47,20 @@ Example: Run  "movie-this" command
        * Plot of the movie.
        * Actors in the movie.
 
-      ![Results](/screenshots/movie-this_results.PNG)
+      ![](screenshots/movie-this_results.PNG)
 
 Example: Run "do-what-it-says" command
 
- $ node liri.js do-what-it-says
+ $ node _liri.js_ do-what-it-says
 
 
  Output:  Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
 
-     ![Results](/screenshots/do-what-it-says_results.PNG)
+     ![](screenshots/do-what-it-says_results.PNG)
     
-TECHNOLOGIES USED
+## TECHNOLOGIES USED
 
 * Nodejs
 * Javascript
@@ -75,7 +76,7 @@ TECHNOLOGIES USED
     - OMDB
 * GitHub   
 
-References: Youtube videos, GitHub repositories and UM Coding Boot Camp instructions
+*References: Youtube videos, GitHub repositories and UM Coding Boot Camp instructions*
 
 
 
--- liri.js
+++ liri.js
@@ -19,30 +19,32 @@ var getArtistNames = function (artist) {
     return artist.name;
 }
 //Function for Concert Info: Bands in Town
-function showConcertInfo(inputParameter){
+function showConcertInfo(inputParameter) {
+    console.log(inputParameter);
     var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
-    request(queryUrl, function(error, response, body) {
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-        var concerts = JSON.parse(body);
-        for (var i = 0; i < concerts.length; i++) {  
-            console.log("**********EVENT INFO*********");  
-            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");//Append in log.txt file
-            console.log(i);
-            fs.appendFileSync("log.txt", i+"\n");
-            console.log("Name of the Venue: " + concerts[i].venue.name);
-            fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
-            console.log("Venue Location: " +  concerts[i].venue.city);
-            fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
-            console.log("Date of the Event: " +  concerts[i].datetime);
-            fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
-            console.log("*****************************");
-            fs.appendFileSync("log.txt", "*****************************"+"\n");
+    request(queryUrl, function (error, response, body) {
+        // If the request is successful
+        console.log(queryUrl);
+        if (!error && response.statusCode === 200) {
+            var concerts = JSON.parse(body);
+            for (var i = 0; i < concerts.length; i++) {
+                console.log("**********EVENT INFO*********");
+                fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");//Append in log.txt file
+                console.log(i);
+                fs.appendFileSync("log.txt", i + "\n");
+                console.log("Name of the Venue: " + concerts[i].venue.name);
+                fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name + "\n");
+                console.log("Venue Location: " + concerts[i].venue.city);
+                fs.appendFileSync("log.txt", "Venue Location: " + concerts[i].venue.city + "\n");
+                console.log("Date of the Event: " + concerts[i].datetime);
+                fs.appendFileSync("log.txt", "Date of the Event: " + concerts[i].datetime + "\n");
+                console.log("*****************************");
+                fs.appendFileSync("log.txt", "*****************************" + "\n");
+            }
+        } else {
+            console.log('Error occurred.');
         }
-    } else{
-      console.log('Error occurred.');
-    }
-});
+    });
 }
 
 var getMeSpotify = function (songName) {
@@ -52,7 +54,7 @@ var getMeSpotify = function (songName) {
             return console.log('Error occurred: ' + err);
             return;
         }
-    
+
         var songs = data.tracks.items;
         for (var i = 0; i < songs.length; i++) {
             console.log(i);
@@ -65,14 +67,14 @@ var getMeSpotify = function (songName) {
             console.log('****************************');
         }
     });
-} 
+}
 
 
-var getMeMovie = function(movieName){
-    request('http://omdbapi.com/?t='+ movieName + '&y=&plot=short&apikey=trilogy', function(error, response, body){
-        if(!error && response.statusCode ==200){
+var getMeMovie = function (movieName) {
+    request('http://omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
-            var jsonData =JSON.parse(body);
+            var jsonData = JSON.parse(body);
 
             console.log('Title:' + jsonData.Title);
             console.log('Year:' + jsonData.Year);
@@ -87,39 +89,52 @@ var getMeMovie = function(movieName){
         }
     });
 }
- var doWhatItSays = function() {
-fs.readFile('random.txt', 'utf8', function(err, data){
-    if (err) throw err;
-    
-    var dataArr = data.split(',');
+var doWhatItSays = function () {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
 
-    if(dataArr.length == 2){
-       pick(dataArr[0], dataArr[1]); 
-    } else if (dataArr.length == 1){
-        pick(dataArr[0]);
-    }
-    console.log(data);
-});
+        console.log(data);
+        if (err) throw err;
+
+        var dataArr = data.split(',');
+
+        if (dataArr.length == 2) {
+
+            pick(dataArr[0], dataArr[1]);
+            //    pick(dataArr[0], "Madonna"); 
+        } else if (dataArr.length == 1) {
+            pick(dataArr[0]);
+        }
+        console.log(data);
+    });
 }
 var pick = function (caseData, functionData) {
-        switch (caseData) {
-            case 'spotify-this-song':
-                getMeSpotify(functionData);
-                break; 
-            case 'concert-this':
-                showConcertInfo(functionData);
-                break; 
-            case 'movie-this':   
-                getMeMovie(functionData);
-                break;
-            case 'do-what-it-says':   
-                doWhatItSays();
-                break;
-                default:
-                console.log('LIRI does not know that');
+    console.log(caseData);
+    switch (caseData) {
+        case 'spotify-this-song':
+                console.log("1");
 
-        }
+            getMeSpotify(functionData);
+            break;
+        case 'concert-this':
+                console.log("2");
+
+            showConcertInfo(functionData);
+            break;
+        case 'movie-this':
+                console.log("3");
+
+            getMeMovie(functionData);
+            break;
+        case 'do-what-it-says':
+            console.log("4");
+
+            doWhatItSays();
+            break;
+        default:
+            console.log('LIRI does not know that');
 
     }
+
+}
 
 pick(process.argv[2], process.argv[3]);
